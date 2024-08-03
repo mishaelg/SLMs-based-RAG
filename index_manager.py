e98226ae189bba89eec8b0f4b7f8885a34325880
Mishael <mishael@xFind.ai        user = mishael>
2024-08-03 17:17:19 +0300

    Improved the code quality

--- index_manager.py
+++ index_manager.py
@@ -2,6 +2,8 @@ import faiss
 import numpy as np
 import pickle
 
+from .settings import INDEX_NAME, INDEX_MAPPER_NAME
+
 
 class SingleIndexManager:
     def __init__(self, embedding_dim):
@@ -87,8 +89,12 @@ class SingleIndexManager:
             self.index_to_doc_id = pickle.load(f)
 
 
-def index_corpus(corpus, model, model_manager):
+def index_corpus(corpus, model, model_manager, index_output=None):
     docs_embeddings = model.encode(
         [doc['title'] + ' ' + doc['text'] for doc in corpus.values()])
     docs_ids = list(corpus.keys())
     model_manager.add_documents(docs_embeddings, docs_ids)
+    if index_output:
+        index_output.mkdir(parents=True, exist_ok=True)
+        model_manager.save_index(
+            index_output / INDEX_NAME, index_output / INDEX_MAPPER_NAME)
--- tinies_vs_giant.py
+++ tinies_vs_giant.py
@@ -1,13 +1,18 @@
+import logging
 from beir import util
 from beir.datasets.data_loader import GenericDataLoader
+from sentence_transformers import SentenceTransformer
+from pathlib import Path
 
 
+from .eval import get_queries_results
+from .index_manager import SingleIndexManager, index_corpus
 from .utils import save_json, load_json, load_yaml
+from .configs import Config
+from .settings import (CONFIG_FILE, DATASETS_FOLDER, RESULTS_FOLDER, INDEX_FOLDER, LOGS_FOLDER,
+                       QUERIES_RESULTS_FILE)
 
-
-CONFIG_FILE = "config.yaml"
-DATASETS_FOLDER = "datasets"
-CONFIGS = load_yaml(CONFIG_FILE)
+logger = logging.getLogger('tinies_vs_giant')
 
 
 def download_dataset(dataset_name, out_dir):
@@ -24,25 +29,49 @@ def get_dataset(dataset_name, out_dir):
     return corpus, queries, qrels
 
 
+def get_model_query_result_on_dataset(model, queries, model_manager, output_path):
+    queries_result_path = output_path / QUERIES_RESULTS_FILE
+    results = {}
+    for query_id, query in queries.items():
+        query_embedding = model.encode(query)
+        results[query_id] = model_manager.search(query_embedding)
+    save_json(results, queries_result_path)
+
+
+def calculate_dataset_performance(dataset_name, configs):
+    dataset_output_folder = configs.output_folder / dataset_name
+    dataset_output_folder.mkdir(parents=True, exist_ok=True)
+    corpus, queries, qrels = get_dataset(
+        dataset_name, configs.output_folder)
+    # first get the results for all slm models
+    for model_name in configs.slm_models_names:
+        model_eval(configs, corpus, queries, model_name)
+    # then get the results for the llm model
+    model_eval(configs, corpus, queries, configs.llm_model_name)
+    # finally calculate the metrics
+    calculate_metrics(configs, dataset_output_folder, qrels)
+
+
+def model_eval(configs, corpus, queries, model_name):
+    logger.info("Calculating results for %s", model_name)
+    model_output_folder = configs.output_folder / model_name
+    model_output_folder.mkdir(parents=True, exist_ok=True)
+    model = SentenceTransformer(model_name)
+    model_manager = SingleIndexManager(
+        model.get_sentence_embedding_dimension())
+    indexes_path = configs.output_folder / \
+        INDEX_FOLDER if configs.save_index else None
+    index_corpus(corpus, model, model_manager, indexes_path)
+    get_model_query_result_on_dataset(
+        model, queries, model_manager, model_output_folder)
+
 
 def main():
-    for dataset_name in CONFIGS["datasets"]:
-        corpus, queries, qrels = get_dataset(
-            dataset_name, CONFIGS["data_path"])
-        print("Corpus size:", len(corpus))
-        print("Queries size:", len(queries))
-        print("Qrels size:", len(qrels))
-    datasets_folder = CONFIGS["datasets_folder"]    
-    corpus, queries, qrels = get_dataset(
-        CONFIGS["dataset_name"], CONFIGS["data_path"])
-
-    # Load the model
-    model_manager = ModelManager()
-    model = model_manager.get_model(CONFIGS["model_name"])
-
-    # Index the corpus
-    index_manager = IndexManager()
-    index_manager.index_corpus(corpus, model, model_manager)
-
-    # Get the results for the queries
-    get_queries_results(queries, index_manager, CONFIGS["output_path"], model)
+    logger.info("Initializing the experiment")
+    configs_data = load_yaml(CONFIG_FILE)
+    configs = Config(**configs_data)
+    configs.output_folder = Path(configs.output_folder)
+    for dataset_name in configs.datasets:
+        logger.info("Running the experiment on %s", dataset_name)
+        calculate_dataset_performance(dataset_name, configs)
+        logger.info("Finished the experiment on %s", dataset_name)
